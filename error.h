31e10d179284dc43ac1cfc8e354e1718a4f71456
Denis Novosád <60551739+PendasDreams@users.noreply.github.com>
2022-11-23 12:56:56 +0100

    initial commit

---
+++ error.h
@@ -0,0 +1,28 @@
+/**
+ * @file error.c
+ * @author xnovos14
+ * @author 
+ * @brief Error codes
+ */
+
+
+#include <stdio.h>
+
+
+/**
+ * @brief Chybové kódy překladače
+ */
+
+
+typedef enum error_code {
+    ERROR_OK = 69, // není chyba
+    ERROR_LEX = 1,
+    ERROR_SYNTAX = 2,
+    ERROR_SEM_DEF = 3,
+    ERROR_SEM_PARAM = 4,
+    ERROR_SEM_UNDEF_VAR = 5,
+    ERROR_SEM_FUNC_RET = 6,
+    ERROR_SEM_TYPE = 7,
+    ERROR_SEM_OTHER = 8,
+    ERROR_PROGRAM = 99, 
+} error_code;
