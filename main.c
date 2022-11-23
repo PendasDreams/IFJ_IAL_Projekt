ef66f4ea03f1b91362885db159781424018f7ceb
Denis Novosád <60551739+PendasDreams@users.noreply.github.com>
2022-11-23 12:54:42 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+/**
+ * @file main.c
+ * @author xnovos14
+ * @author 
+ * @brief Main
+ */
+
+#include <stdio.h>
+#include "scanner.h"
+#include "error.h"
+
+
+int main(int argc, char const *argv[])
+{
+	FILE* file = stdin;
+	if (argc == 2) {
+		file = fopen(argv[1], "r");
+	}
+	if (file == NULL) {
+		file = stdin;
+	}
+
+    TOKEN token;
+    SpracujZnak(&token);
+    fclose(file);
+    
+	return 0;
+}
